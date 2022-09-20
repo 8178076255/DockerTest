287800280ad55314074d3c8a46dadced9f6e83f4
Binod kumar <srp.binod@gmail.com>
2022-09-20 10:27:17 +0530

    Test message

--- src/main/java/com/example/dockertest/DockerTestApplication.java
+++ src/main/java/com/example/dockertest/DockerTestApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerTestApplication {
 
+	//Rest controller
 	@GetMapping("/hello/{name}")
 	public String syaHello(@PathVariable String name) {
 		return "Hello " + name;
